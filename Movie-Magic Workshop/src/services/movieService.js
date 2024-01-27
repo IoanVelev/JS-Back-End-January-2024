f21cc64d092579511cb1f6cadb1b7bf81634c958
Ioan Velev <ioanvelev46@gmail.com>
2024-01-27 23:55:49 +0200

    Show dynamic movies on home page

--- Movie-Magic Workshop/src/services/movieService.js
+++ Movie-Magic Workshop/src/services/movieService.js
@@ -1,6 +1,18 @@
-const movies = [];
+const movies = [{
+    title: 'Jungle Cruise',
+    genre: 'Adventure',
+    director: 'Spilberg',
+    date: '2020',
+    imageUrl: '/img/jungle-cruise.jpeg',
+    rating: '5',
+    description: 'cool movie'
+}];
 
 
+exports.getAll = () => {
+return movies.slice();
+}
 exports.create = (movieData) => {
     movies.push(movieData);
 }
+
