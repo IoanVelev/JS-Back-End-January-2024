1a502166d3cf1211979caf6331b33324860b4d37
Ioan Velev <ioanvelev46@gmail.com>
2024-02-01 22:10:11 +0200

    Create and save cast to db

--- Movie-Magic Workshop/src/controllers/castController.js
+++ Movie-Magic Workshop/src/controllers/castController.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 
+const Cast = require('../services/castService');
+
 router.get('/create', (req, res) => {
     res.render('cast/create');
 });
 
-router.post('/create', (req, res) => {
-    const body = req.body;
-    console.log(body);
-
-    res.redirect('/');
-})
+router.post('/create', async (req, res) => {
+    const castData = req.body;
+    
+    try {
+        await Cast.create(castData);
+        res.redirect('/');
+    } catch (err) {
+        res.redirect('/cast/create');
+    }
+});
 
 module.exports = router;
