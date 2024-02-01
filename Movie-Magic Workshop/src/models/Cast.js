0a1522e3384f3ccb8f2593d47e4b413e218b1212
Ioan Velev <ioanvelev46@gmail.com>
2024-02-01 21:45:42 +0200

    Create cast model

---
+++ Movie-Magic Workshop/src/models/Cast.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const castSchema = new mongoose.Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    age: {
+        required: true,
+        type: Number,
+        min: 13,
+        max: 90
+    },
+    born: {
+        required: true,
+        type: String
+    },
+    nameInMovies: {
+        required: true,
+        type: String
+    },
+    castImage: {
+        required: true,
+        type: String,
+        validate: {
+            validator(value){
+               return /^https?:\/\//.test(value);
+            },
+            message: (props) => `${props.value} is invalid URL for the cast image`
+        }
+    },
+});
+
+const Cast = mongoose.model('Cast', castSchema);
+
+module.exports = Cast;
