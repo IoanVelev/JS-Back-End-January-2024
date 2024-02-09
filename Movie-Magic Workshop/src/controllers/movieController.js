f36d8f1184e7235527869350a320adaabf1532cd
Ioan Velev <ioanvelev46@gmail.com>
2024-02-09 16:39:30 +0200

    Display accurate movie data on edit page

--- Movie-Magic Workshop/src/controllers/movieController.js
+++ Movie-Magic Workshop/src/controllers/movieController.js
@@ -41,15 +41,19 @@ router.get('/movies/:movieId/attach/cast', async (req, res) => {
 
 router.post('/movies/:movieId/attach/cast', async (req, res) => {
     const castId = req.body.cast;
-    const movieId = req.params.movieId
+    const movieId = req.params.movieId;
 
     await movieService.attach(movieId, castId);
 
     res.redirect(`/movies/${movieId}/attach/cast`);
 });
 
-router.get('/movies/:movieId/edit', (req, res) => {
-    res.render('movies/edit');
-});
+router.get('/movies/:movieId/edit', async (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = await movieService.getOne(movieId).lean();
+
+    console.log(movie);
+    res.render('movies/edit', { movie });
+}); 
 
 module.exports = router;
