fb4516d0dec56c185a4567617d814af18ee8a88b
Ioan Velev <ioanvelev46@gmail.com>
2024-02-11 20:54:35 +0200

    Show error message on login page when we have such

--- Movie-Magic Workshop/src/controllers/authController.js
+++ Movie-Magic Workshop/src/controllers/authController.js
@@ -27,11 +27,18 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
-    const token = await authService.login(email, password);
     
-    res.cookie('auth', token);
 
-    res.redirect('/');
+    try {
+        const token = await authService.login(email, password);
+
+        res.cookie('auth', token);
+
+        res.redirect('/');
+    } catch (err) {
+        const message = getErrorMessage(err);
+        res.status(400).render('auth/login', { error: message });
+    }
 });
 
 router.get('/logout', (req, res) => {
