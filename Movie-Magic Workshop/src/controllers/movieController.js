ef763ed34159f297db840a6e6ce829e09e49a033
Ioan Velev <ioanvelev46@gmail.com>
2024-02-11 18:58:21 +0200

    Show error on create movie page, when we have such

--- Movie-Magic Workshop/src/controllers/movieController.js
+++ Movie-Magic Workshop/src/controllers/movieController.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 const movieService = require('../services/movieService');
 const castService = require('../services/castService');
-const { isAuth } = require('../middlewares/authMiddleware')
+const { isAuth } = require('../middlewares/authMiddleware');
+const { getErrorMessage } = require('../utils/errorUitls');
 
 router.get('/create', isAuth, (req, res) => {
     res.render('create');
@@ -18,8 +19,8 @@ router.post('/create', isAuth, async (req, res) => {
         await movieService.create(newMovie);
         res.redirect('/');
     } catch (err) {
-        console.log(err.message);
-        res.redirect('/create')
+        const message = getErrorMessage(err);
+        res.status(400).render('create', { error: message, newMovie })
     }
 });
 
