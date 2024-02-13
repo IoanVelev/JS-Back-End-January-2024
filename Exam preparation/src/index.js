bff00abdfe79b6768764aada74a7caa24543f983
Ioan Velev <ioanvelev46@gmail.com>
2024-02-13 15:31:46 +0200

    Add static middleware and body parser

--- Exam preparation/src/index.js
+++ Exam preparation/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Home page');
