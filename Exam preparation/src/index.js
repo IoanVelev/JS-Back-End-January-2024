cc9f9f220473cca127b23929e0e18856c59c1f74
Ioan Velev <ioanvelev46@gmail.com>
2024-02-13 15:45:13 +0200

    Create routes module

--- Exam preparation/src/index.js
+++ Exam preparation/src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve('src/public')));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('Home page');
-});
+app.use(routes);
+
 
 
 app.listen(3000, () => console.log('Server is listening on port: http://localhost:3000'));
