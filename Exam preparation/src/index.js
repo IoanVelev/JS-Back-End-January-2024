d5d1992c82fc7c293d1c8c78500ee71c4e0817bb
Ioan Velev <ioanvelev46@gmail.com>
2024-02-14 17:29:57 +0200

    Update index.js

--- Exam preparation/src/index.js
+++ Exam preparation/src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
 const configExpress = require('./config/configExpress');
 const configHandlebars = require('./config/configHandlebars');
