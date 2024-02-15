8061d5e653d2b7216a85d64865042a0aed0cb198
Ioan Velev <ioanvelev46@gmail.com>
2024-02-15 21:51:36 +0200

    Render details page based on requested course
    Details page is visualized based on the requested course

--- Exam preparation/src/controllers/courseController.js
+++ Exam preparation/src/controllers/courseController.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const courseService = require('../services/courseService');
+const userService = require('../services/userService');
 const { getErrorMessage } = require('../utils/errorUtils');
 
 router.get('/', async (req, res) => {
     const courses = await courseService.getAll().lean();
     res.render('courses/catalog', { courses });
-})
+});
 
 router.get('/create', isAuth, (req, res) => {
     res.render('courses/create');
@@ -22,6 +23,14 @@ router.post('/create', isAuth, async (req, res) => {
         res.render('courses/create', { ...courseData, error: getErrorMessage(err) });
     }
 
+});
+
+router.get('/:courseId/details', async (req, res) => {
+    const course = await courseService.getOne(req.params.courseId).lean();
+
+    const owner = await userService.getUser(course.owner);
+
+    res.render('courses/details', { ...course, owner: owner.email });
 });
 
 module.exports = router;
